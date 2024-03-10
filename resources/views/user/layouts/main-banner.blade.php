6506130eab5d2d257f506d4b2434137a89c81361
Lionel Vo <vovanmai.dt3@gmail.com>
2024-03-10 16:22:55 +0700

    interface

--- resources/views/user/layouts/main-banner.blade.php
+++ resources/views/user/layouts/main-banner.blade.php
@@ -1,15 +1,9 @@
 @if($isHomePage ?? false)
     <!-- Main banner -->
-        <div class="swiper main-banner-swiper">
+    <div class="swiper main-banner-swiper">
         <div class="swiper-wrapper">
             <div class="swiper-slide">
                 <img src="https://www.pace.edu.vn/uploads/banners/pace-institute-of-management-vie.jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="">
-            </div>
-            <div class="swiper-slide">
-                <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="">
             </div>
             <div class="swiper-slide">
                 <img src="https://resize.sudospaces.com/ketoanleanh/2020/08/khoa-hoc-ke-toan-tong-hop-1.jpg.webp" alt="">
