ab3209a3ba9a6c0dde24984daaf553f7166666be
Vo Van Mai <mai.vv@neo-lab.vn>
2024-04-02 10:24:32 +0700

    refactor

--- app/Services/User/IndexService.php
+++ app/Services/User/IndexService.php
@@ -61,6 +61,7 @@ class IndexService
 
             $category->activePosts = $this->repository->whereIn('category_id', $catIds)
                 ->whereByField('active', true)
+                ->orderBy('id', 'DESC')
                 ->limit(6);
         }
 
