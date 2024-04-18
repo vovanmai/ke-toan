0f564cc13494891b0d683f2974697e8b78645621
Lionel Vo <vovanmai.dt3@gmail.com>
2024-04-18 16:29:06 +0700

    phone

--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
     <meta name="google-site-verification" content="jF7ooTqh583AJEnuZ1M2DDrSrCarnf2NhiXe0-jkE94">
+    <meta name="format-detection" content="telephone=no">
     {!! \Artesaos\SEOTools\Facades\SEOMeta::generate() !!}
     {!! \Artesaos\SEOTools\Facades\OpenGraph::generate() !!}
     @if($link = app('web_setting')->link_fan_page_facebook ?? null)
