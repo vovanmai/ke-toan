84538bb192ee95773653f1ec623dc50bfd9ea5a4
Lionel Vo <vovanmai.dt3@gmail.com>
2024-04-19 19:56:35 +0700

    SEO

--- config/seotools.php
+++ config/seotools.php
@@ -9,9 +9,9 @@ return [
          * The default configurations to be used by the meta generator.
          */
         'defaults'       => [
-            'title'        => 'Kế toán DPT: https://www.ketoandpt.com', // set false to total remove
+            'title'        => 'Đào tạo kế toán - Đại lý thuế DPT - Công ty cổ phần tổng hợp DPT - https://www.ketoandpt.com', // set false to total remove
             'titleBefore'  => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
-            'description'  => 'Trung tâm kế toán chuyên nghiệp ', // set false to total remove
+            'description'  => 'Đào tạo kế toán - Đại lý thuế DPT - Công ty cổ phần tổng hợp DPT - https://www.ketoandpt.com', // set false to total remove
             'separator'    => ' - ',
             'keywords'     => [],
             'canonical'    => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
@@ -36,11 +36,11 @@ return [
          * The default configurations to be used by the opengraph generator.
          */
         'defaults' => [
-            'title'       => '', // set false to total remove
-            'description' => '', // set false to total remove
+            'title'       => 'Đào tạo kế toán - Đại lý thuế DPT - Công ty cổ phần tổng hợp DPT - https://www.ketoandpt.com', // set false to total remove
+            'description' => 'Đào tạo kế toán - Đại lý thuế DPT - Công ty cổ phần tổng hợp DPT - https://www.ketoandpt.com', // set false to total remove
             'url'         => null, // Set null for using Url::current(), set false to total remove
             'type'        => '',
-            'site_name'   => 'Trung tâm đào tạo kếDPT toán chuyên nghiệp DPT',
+            'site_name'   => 'Đào tạo kế toán - Đại lý thuế DPT - Công ty cổ phần tổng hợp DPT - https://www.ketoandpt.com',
             'images'      => [],
             'locale' => 'vi_VN',
         ],
@@ -51,7 +51,7 @@ return [
          */
         'defaults' => [
             'card'        => 'summary',
-            'site'        => 'Trung tâm đào tạo kếDPT toán chuyên nghiệp DPT',
+            'site'        => 'Đào tạo kế toán - Đại lý thuế DPT - Công ty cổ phần tổng hợp DPT - https://www.ketoandpt.com',
         ],
     ],
     'json-ld' => [
@@ -59,8 +59,8 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => true, // set false to total remove
-            'description' => '', // set false to total remove
+            'title'       => 'Đào tạo kế toán - Đại lý thuế DPT - Công ty cổ phần tổng hợp DPT - https://www.ketoandpt.com', // set false to total remove
+            'description' => 'Đào tạo kế toán - Đại lý thuế DPT - Công ty cổ phần tổng hợp DPT - https://www.ketoandpt.com', // set false to total remove
             'url'         => null, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
             'type'        => '',
             'images'      => [],
